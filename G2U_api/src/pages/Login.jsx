60fac4240b17868fc510fa78aadd00af5521cb91
Tani <vtvarma2012@gmail.com>
2025-07-02 15:12:41 +0530

    Initial commit for G2U_API_demo project

---
+++ G2U_api/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='flex items-center justify-center'>
+      <h1>Login Page</h1>
+      <p>Welocmes back</p>
+    </div>
+  )
+}
+
+export default Login
